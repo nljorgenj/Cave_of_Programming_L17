181768b0adc82760d2d12f0005cf8e1fca2c6342
nljorgenj <nljorgenj@gmail.com>
2017-02-28 21:52:59 +0100

    complex conditions

--- complex conditions/complex conditions.cpp
+++ complex conditions/complex conditions.cpp
@@ -19,16 +19,38 @@ int main() {
 
 	if (value1 >= 5) {
 		cout << "condition 1: true" << endl;
-	}
-	else {
+	} else {
 		cout << "Condition 1: false" << endl;
 	}
 
 	if (value1 == 7 && value2 < 3) {
 		cout << "condition 2: true" << endl;
-	}
-	else {
+	} else {
 		cout << "Condition 2: false" << endl;
+	}
+
+	if (value1 == 7 || value2 < 3) {
+		cout << "condition 3: true" << endl;
+	} else {
+		cout << "Condition 3: false" << endl;
+	}
+
+	if ( (value2 != 8 && value1 == 7)  || value1 < 10) {
+		cout << "condition 4: true" << endl;
+	} else {
+		cout << "Condition 4: false" << endl;
+	}
+
+	bool condition1 = (value2 != 8) && (value1 == 10);
+	cout << condition1 << endl;
+
+	bool condition2 = value1 < 10;
+	cout << condition2 << endl;
+
+	if (condition1 || condition2) {
+		cout << "condition 5: true" << endl;
+	} else {
+		cout << "Condition 5: false" << endl;
 	}
 
 
